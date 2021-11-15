e1581c75ab5cbd2ee06e33a4bb75f7202ff80d1c
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2021-11-15 23:28:07 +0900

    create reducer for events

---
+++ src/reducers/index.ts
@@ -0,0 +1,38 @@
+type State = [] | [{ id: number; title: string; body: string }];
+
+type CreateAction = {
+  type: "CREATE_EVENT";
+  title: string;
+  body: string;
+};
+
+type DeleteAction = {
+  type: "DELETE_EVENT";
+};
+
+type DeleteAllAction = {
+  type: "DELETE_ALL_EVENT";
+};
+
+type Actions = CreateAction | DeleteAction | DeleteAllAction;
+
+const events = (state: State, action: Actions) => {
+  switch (action.type) {
+    case "CREATE_EVENT":
+      const event = { title: action.title, body: action.body };
+      const length = state.length;
+      const id = length === 0 ? 1 : state[length - 1].id + 1;
+      return [...state, { id, ...event }];
+
+    case "DELETE_EVENT":
+      return state;
+
+    case "DELETE_ALL_EVENT":
+      return [];
+
+    default:
+      return state;
+  }
+};
+
+export default events;
